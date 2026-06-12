2d997db355eb1f771ad692108faba4e58622d1fe
Guilherme Correa <gc.ribeiro88@gmail.com>
2026-06-12 14:41:22 -0300

    aumentar numero

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+paragrafo.innerHTML = 'Escolha um número entre 1 e 100';
 
 function verificarChute() {
     console.log('o botão foi clicado');
